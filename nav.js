96945cd3ba197f7a4b0601a5e026cca2244a04c9
cAPSlOCKGames <83343855+cAPSlOCKGames@users.noreply.github.com>
2021-05-02 16:47:52 +0300

    Added navigation tab

---
+++ nav.js
@@ -0,0 +1,9 @@
+function show(){
+    var nav = document.getElementById("navigation")
+    nav.style.visibility = "visible"
+}
+
+function hide(){
+    var nav = document.getElementById("navigation")
+    nav.style.visibility = "hidden"
+}
