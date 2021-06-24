04524a34fe3ead61d4d6044fd7fad82c354f130f
cAPSlOCKGames <83343855+cAPSlOCKGames@users.noreply.github.com>
2021-06-24 11:17:15 +0300

    fixed size of navigationbar

--- nav.js
+++ nav.js
@@ -1,16 +1,19 @@
 
 var a;
 
-function navbtn(nav, link){
+function navbtn(nav, link, brhide){
     link = document.getElementById("links")
     nav = document.getElementById("navigationbar")
+    brhide = document.getElementById("hidebr")
     if (a == 1){
         nav.style.display = "none"
         link.style.display = "none"
+        brhide.style.display = "none"
         return a = 0
     } else{
         nav.style.display = "block"
         link.style.display = "block"
+        brhide.style.display = "block"
         return a = 1
     }
 }
